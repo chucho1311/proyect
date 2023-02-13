96e266e3aa3b3d5f9acfaa191abe6809f4c8244a
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 15:23:09 -0500

    cnaosj

--- src/app.js
+++ src/app.js
@@ -6,14 +6,9 @@ import cors from "cors";
 const app = express()
 Headers
 app.use(express.json())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-app.use(cors())
+app.use(cors({
+    origin: whiteList
+}))
 
 app.use("/api/v1", usersRoutes)
 app.use("/api/v1", restaurantRoutes)

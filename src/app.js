8fbfbe9260403438b308ab8defd17fe0fdd69fa6
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 18:57:11 -0500

    cojsnoa

--- src/app.js
+++ src/app.js
@@ -5,13 +5,7 @@ import cors from "cors";
 
 const app = express()
 app.use(express.json())
-app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-})
+// app.use(cors())
 
 app.use("/api/v1", usersRoutes)
 app.use("/api/v1", restaurantRoutes)

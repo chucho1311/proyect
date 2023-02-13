115a403c9b5f7d99e1df69fdd9997cfd1642e632
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 13:42:02 -0500

    import cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import usersRoutes from "./users/usersRoutes.js";
 import restaurantRoutes from "./restaurants/restaurantsRoutes.js"
+import cors from "cors";
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/v1",usersRoutes)
 app.use("/api/v1",restaurantRoutes)

55a9c5be2ca1cd858e7247ab307f1597c8085beb
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 15:17:16 -0500

    cors bug

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ import restaurantRoutes from "./restaurants/restaurantsRoutes.js"
 import cors from "cors";
 
 const app = express()
+Headers
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use(cors())
 
-app.use("/api/v1",usersRoutes)
-app.use("/api/v1",restaurantRoutes)
+app.use("/api/v1", usersRoutes)
+app.use("/api/v1", restaurantRoutes)
 // app.get('/', async (req,res) => {
 //     const result =await pool.query('SELECT 1 + 1 AS result')
 //     res.send(result)

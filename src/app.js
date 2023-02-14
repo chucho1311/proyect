3b20c4c2bf578725f551c15b0a1f34e9ea26850f
chucho1311 <carlosscristancho69@gmail.com>
2023-02-14 01:13:37 -0500

    npk

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors({ credentials: true, origin: 'https://la-miapizza.netlify.app/', cr
 app.use(express.json())
 app.use( (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Accept");
     next();
 });
 app.use("/api/v1", usersRoutes)

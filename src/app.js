5609a098251b5cd32c566bcbe6ddcdf105a68f23
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 20:54:51 -0500

    nojasnd

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import cors from "cors";
 
 const app = express()
 
-app.use(cors({credentials: true, origin: '*'}));
+app.use(cors({credentials: true, origin: 'https://la-miapizza.netlify.app/'}));
 app.use(express.json())
 
-app.use("/api/v1",cors({credentials: true, origin: '*'}), usersRoutes)
+app.use("/api/v1", usersRoutes)
 app.use("/api/v1", restaurantRoutes)
 // app.get('/', async (req,res) => {
 //     const result =await pool.query('SELECT 1 + 1 AS result')

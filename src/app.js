dc2ee7235a6c90047030385e00d50f6317f96f40
chucho1311 <carlosscristancho69@gmail.com>
2023-02-13 20:32:21 -0500

    ojnond

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 const app = express()
 
-app.use(cors({credentials: true, origin: 'localhost:3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000/'}));
 app.use(express.json())
 
 app.use("/api/v1", usersRoutes)
